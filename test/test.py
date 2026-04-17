6529876ed865f58cb44e949a16cde2fcb24fa3d3
ag <76513830+arfanghani@users.noreply.github.com>
2026-04-17 17:32:33 +0400

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,11 +1,11 @@
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import RisingEdge, ClockCycles
-
+from cocotb.triggers import RisingEdge
 
 @cocotb.test()
 async def test_heat_alert(dut):
 
+    # FIXED LINE
     clock = Clock(dut.clk, 20, units="ns")
     cocotb.start_soon(clock.start())
 
@@ -14,20 +14,18 @@ async def test_heat_alert(dut):
     dut.ui_in.value = 0
     dut.uio_in.value = 0
 
-    await ClockCycles(dut.clk, 2)
+    for _ in range(5):
+        await RisingEdge(dut.clk)
+
     dut.rst_n.value = 1
 
-    # generate pulses (simulate FNIRSI frequency)
-    for i in range(30):
+    for i in range(40):
         dut.ui_in.value = 1
         await RisingEdge(dut.clk)
 
         dut.ui_in.value = 0
         await RisingEdge(dut.clk)
 
-        state = int(dut.uo_out.value)
-        dut._log.info(f"Cycle {i} | State={state}")
-
-    # clean finish
-    dut.ui_in.value = 0
-    await ClockCycles(dut.clk, 5)
+        if dut.uo_out.value.is_resolvable:
+            state = int(dut.uo_out.value)
+            dut._log.info(f"step={i} state={state}")
